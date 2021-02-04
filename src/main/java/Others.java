f82ec55ec29a2d9cb14acab3aded65c1445a306c
VergeDX <neko@hydev.org>
2021-02-04 22:19:15 +0800

    [+] Finish lcci_02.

--- src/main/java/Others.java
+++ src/main/java/Others.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Others {
     // https://leetcode-cn.com/problems/delete-middle-node-lcci/submissions/
     static class ListNode {
@@ -8,9 +11,42 @@ public class Others {
             val = x;
         }
 
+        // https://leetcode-cn.com/problems/delete-middle-node-lcci/
         public static void deleteNode(ListNode node) {
             node.val = node.next.val;
             node.next = node.next.next;
+        }
+
+        // https://leetcode-cn.com/problems/intersection-of-two-linked-lists-lcci/
+        public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode currentNode = headA;
+            while (currentNode != null) {
+                ListNode tempCurrentNode = headB;
+                while (tempCurrentNode != null) {
+                    if (currentNode == tempCurrentNode) return currentNode;
+
+                    tempCurrentNode = tempCurrentNode.next;
+                }
+
+                currentNode = currentNode.next;
+            }
+
+            return null;
+        }
+
+        // https://leetcode-cn.com/problems/linked-list-cycle-lcci/
+        public ListNode detectCycle(ListNode head) {
+            List<ListNode> foredNode = new ArrayList<>();
+
+            ListNode currentNode = head;
+            while (currentNode != null) {
+                if (foredNode.contains(currentNode)) return currentNode;
+
+                foredNode.add(currentNode);
+                currentNode = currentNode.next;
+            }
+
+            return null;
         }
     }
 }
--- src/main/kotlin/lcci_02.kt
+++ src/main/kotlin/lcci_02.kt
@@ -1,3 +1,6 @@
+import ListNode.Companion.joinNode
+import java.math.BigInteger
+
 // https://leetcode-cn.com/problems/remove-duplicate-node-lcci/
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
@@ -24,6 +27,110 @@ class ListNode(var `val`: Int) {
             }
 
             return resultNodeList[0]
+        }
+
+        fun ListNode?.joinNode(x: Int): ListNode {
+            if (this == null) return ListNode(x)
+
+            var currentNode = this
+            while (currentNode != null) {
+                if (currentNode.next == null) break
+                currentNode = currentNode.next
+            }
+
+            // Here, currentNode.next == null.
+            currentNode!!.next = ListNode(x)
+
+            return this
+        }
+
+        // https://leetcode-cn.com/problems/partition-list-lcci/
+        fun partition(head: ListNode?, x: Int): ListNode? {
+            if (head == null) return head
+            if (head.next == null) return head
+
+            var smallPart: ListNode? = null
+            var bigPart: ListNode? = null
+
+            var currentNode = head
+            while (currentNode != null) {
+                val currentVal = currentNode.`val`
+
+                when {
+                    currentVal < x -> smallPart = smallPart.joinNode(currentVal)
+                    else -> bigPart = bigPart.joinNode(currentVal)
+                }
+
+                currentNode = currentNode.next
+            }
+
+            currentNode = smallPart
+            while (currentNode != null) {
+                if (currentNode.next == null) break
+                currentNode = currentNode.next
+            }
+
+            currentNode?.next = bigPart
+            return smallPart ?: bigPart
+        }
+
+        // https://leetcode-cn.com/problems/sum-lists-lcci/
+        fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+            var nl1 = ""
+            var nl2 = ""
+
+            var currentNode = l1
+            while (currentNode != null) {
+                val currentVal = currentNode.`val`
+                nl1 += currentVal.toString()
+                currentNode = currentNode.next
+            }
+            currentNode = l2
+            while (currentNode != null) {
+                val currentVal = currentNode.`val`
+                nl2 += currentVal.toString()
+                currentNode = currentNode.next
+            }
+
+            // Null check.
+            if (nl1.isBlank()) nl1 = "0"
+            if (nl2.isBlank()) nl2 = "0"
+
+            // result  maybe 0.
+            val bnl1 = BigInteger(nl1.reversed())
+            val bnl2 = BigInteger(nl2.reversed())
+            val resultStringReversed = bnl1.add(bnl2).toString().reversed()
+
+            // Then turn result.toString().reversed() -> ListNode.
+            val result = ListNode(resultStringReversed[0].toString().toInt())
+            // if (result.`val`.toString().length == 1) return result
+
+            currentNode = result
+            for (index in resultStringReversed.indices) {
+                // Index == 0, continue.
+                if (index == 0) continue
+
+                val tempNode = ListNode(resultStringReversed[index].toString().toInt())
+                currentNode!!.next = tempNode
+                currentNode = tempNode
+            }
+
+            return result
+        }
+
+        // https://leetcode-cn.com/problems/palindrome-linked-list-lcci/
+        fun isPalindrome(head: ListNode?): Boolean {
+            val InputNumberList = ArrayList<Int>()
+
+            var currentNode = head
+            while (currentNode != null) {
+                val currentVal = currentNode.`val`
+                // if (currentVal < 0) input += "-"
+                InputNumberList += currentVal
+                currentNode = currentNode.next
+            }
+
+            return InputNumberList == InputNumberList.reversed()
         }
     }
 }
@@ -64,4 +171,25 @@ fun main() {
     }
     ListNode.removeDuplicateNodes(example1)
     ListNode.removeDuplicateNodes(example2)
+
+    ListNode(0).joinNode(2)
+    ListNode.partition(example1, 3)
+
+    val nl1 = ListNode(7).apply {
+        next = ListNode(1).apply {
+            next = ListNode(6)
+        }
+    }
+    val nl2 = ListNode(5).apply {
+        next = ListNode(9).apply {
+            next = ListNode(2)
+        }
+    }
+    ListNode.addTwoNumbers(nl1, nl2)
+    ListNode.addTwoNumbers(null, null)
+
+    val negative = ListNode(-129).apply {
+        next = ListNode(-129)
+    }
+    ListNode.isPalindrome(negative)
 }
